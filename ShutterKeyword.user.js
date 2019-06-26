d67a65ba65a4a70a027428c33f09c4ebc3eb96a0
Neomaster <thbangkok@users.noreply.github.com>
2019-06-27 00:19:40 +0700

    Add files via upload

--- ShutterKeyword.user.js
+++ ShutterKeyword.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         ShutterKeyword
 // @namespace
-// @version      1.1
+// @version      2.0
 // @description  Extract the keyword from Shutter Stock Preview Page
 // @author       Naphong
 // @match        https://www.shutterstock.com/*image-photo*
 // @match        https://www.shutterstock.com/*image-vector*
 // @match		 https://www.shutterstock.com/*image-illustration*
-// @copyright    2016, Naphong
+// @copyright    2019, Naphong
 // @require      http://code.jquery.com/jquery-latest.min.js
 // @grant        none
 
@@ -27,11 +27,12 @@ var $j = jQuery.noConflict();
 
 $j(document).ready(function() {
     var a = '';
-    $j.each( $j('.product-page-keywords'), function(i, left) {
-       $j('a', left).each(function() {
+    $j.each( $j('[data-track-label="relatedKeywords"'), function(i, left) {
+       $j('button', left).each(function() {
            a+=$j(this).text()+', ';
        });
     });
-    $j('.product-page-keywords').css({ height: "100px" });
-    $j('.product-page-keywords').html('<div class="row" style="position: relative;height:150px;width:100%;padding: 10px 10px 10px 30px;">' + a + '</div>');
+    //console.log($j('C_a_c'));
+    $j('[class="C_a_c"]').css({ height: "100px" });
+    $j('[class="C_a_c"]').html('<div class="row" style="position: relative;height:150px;width:100%;padding: 10px 10px 10px 30px;">' + a + '</div>');
 });
