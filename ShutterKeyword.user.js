defc22757eb9c677fce52454a307492f592b1cae
Neomaster <thbangkok@users.noreply.github.com>
2019-08-26 14:42:56 +0700

    Fix keyword missing after script activated
    Fix keyword missing after script activated

--- ShutterKeyword.user.js
+++ ShutterKeyword.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         ShutterKeyword
 // @namespace
-// @version      2.0
+// @version      2.1
 // @description  Extract the keyword from Shutter Stock Preview Page
 // @author       Naphong
 // @match        https://www.shutterstock.com/*image-photo*
 // @match        https://www.shutterstock.com/*image-vector*
-// @match        https://www.shutterstock.com/*image-illustration*
+// @match		 https://www.shutterstock.com/*image-illustration*
 // @match        https://www.shutterstock.com/*video*
 // @copyright    2019, Naphong
 // @require      http://code.jquery.com/jquery-latest.min.js
@@ -27,12 +27,15 @@
 var $j = jQuery.noConflict();
 
 $j(document).ready(function() {
-    var a = '';
-    $j.each( $j('[data-track-label="relatedKeywords"'), function(i, left) {
-       $j('button', left).each(function() {
-           a+=$j(this).text()+', ';
-       });
-    });
-    $j('[class="C_a_c"]').css({ height: "100px" });
-    $j('[class="C_a_c"]').html('<div class="row" style="position: relative;height:150px;width:100%;padding: 10px 10px 10px 30px;">' + a + '</div>');
+    setTimeout(function() {
+        var a = '';
+        $j.each( $j('[data-automation="ExpandableKeywordsList_container_div"'), function(i, left) {
+            $j('a', left).each(function() {
+                a+=$j(this).text()+', ';
+            });
+        });
+        console.log(a);
+        $j('[class="C_a_c"]').css({ height: "100px" });
+        $j('[class="C_a_c"]').html('<div class="row" style="position: relative;height:150px;width:100%;padding: 10px 10px 10px 30px;">' + a + '</div>');
+    }, 2000);
 });
