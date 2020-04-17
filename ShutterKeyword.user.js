8330d8c6acbc81bb59b867fc2184f3373e066856
Neomaster <thbangkok@users.noreply.github.com>
2020-04-17 17:37:13 +0700

    Update ShutterKeyword.user.js

--- ShutterKeyword.user.js
+++ ShutterKeyword.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ShutterKeyword
 // @namespace
-// @version      2.2
+// @version      2.3
 // @description  Extract the keyword from Shutter Stock Preview Page
 // @author       Naphong
 // @match        https://www.shutterstock.com/*image-photo*
@@ -29,13 +29,13 @@ var $j = jQuery.noConflict();
 $j(document).ready(function() {
     setTimeout(function() {
         var a = '';
-        $j.each( $j('.oc_x_c'), function(i, left) {
+        $j.each( $j('.C_a_c'), function(i, left) {
             $j('a', left).each(function() {
                 a+=$j(this).text()+', ';
             });
         });
         console.log(a);
-        $j('[class="oc_x_g"]').css({ height: "100px" });
-        $j('[class="oc_x_g"]').html('<div class="row" style="position: relative;height:150px;width:100%;padding: 10px 10px 10px 10px;">' + a + '</div>');
+        $j('.k_b_O.k_b_gp.k_b_fX.k_b_ha.k_b_gP.k_b_iB.k_b_iw.k_b_b.m_g_p.section-spacing-bottom').css({ height: "100px" });
+        $j('.k_b_O.k_b_gp.k_b_fX.k_b_ha.k_b_gP.k_b_iB.k_b_iw.k_b_b.m_g_p.section-spacing-bottom').html('<div class="row" style="position: relative;height:150px;width:100%;padding: 10px 10px 10px 10px;">' + a + '</div>');
     }, 2000);
 });
